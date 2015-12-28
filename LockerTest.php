617029094b3fd9126cd65c2ee004d1031c4b0e09
capturepointer <capturepointer@foxmail.com>
2015-12-29 00:02:31 +0800

    fix test php file

--- LockerTest.php
+++ LockerTest.php
@@ -1,7 +1,10 @@
 <?php
 require_once dirname(__FILE__) . "/Locker.inc";
-if(!locker_acquire("lockName",3)){
+if(!locker_acquire("lockName",10.0)){
 	if(!locker_wait("lockName",3)){
+		return false;
+	}
+	else if(!locker_acquire("lockName",10.0)){
 		return false;
 	}
 }
